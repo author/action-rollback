731b50f8cc503d884350800d75cb0882fffdd255
Corey Butler <corey@coreybutler.com>
2020-01-29 12:43:25 -0600

    Remove tag response

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function run() {
       ref: `tags/${data.tag_name}`
     })
 
-    core.debug(JSON.stringify(tagresponse, null, 2))
+    // core.debug(JSON.stringify(tagresponse, null, 2))
 
     core.setOutput('release_id', data.id)
     core.setOutput('tag', data.tag_name)
