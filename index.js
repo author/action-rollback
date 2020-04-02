c673097c115e832eb0514938992d98b2794a30f7
Corey Butler <corey@coreybutler.com>
2020-04-01 21:44:50 -0500

    Added support for removing orhpan tags, even if there is no associated release.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const core = require('@actions/core');
-const { GitHub, context } = require('@actions/github');
+const core = require('@actions/core')
+const { GitHub, context } = require('@actions/github')
 
-async function run() {
+async function run () {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
     const github = new GitHub(process.env.GITHUB_TOKEN)
@@ -29,6 +29,20 @@ async function run() {
 
       if (!data) {
         core.debug(JSON.stringify(data, null, 2))
+
+        if (core.getInput('always_delete_tag', { required: false }) !== '') {
+          const deleteTagResponse = await github.git.deleteRef({
+            owner,
+            repo,
+            ref: `tags/${data.tag_name}`
+          })
+
+          if (deleteTagResponse) {
+            core.warning(`Removed ${data.tag_name}, even though there was no associated release.`)
+            return
+          }
+        }
+
         core.setFailed(`Tag "${tag}" was not found or a release ID is not associated with it.`)
         return
       }
@@ -51,7 +65,7 @@ async function run() {
     }
 
     core.debug(JSON.stringify(data, null, 2))
-    
+
     // API Documentation: https://developer.github.com/v3/repos/releases/#delete-a-release
     // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-delete-release
     core.debug(`Removing release ${data.id}`)
